1d53a7883903d89e2a16d8e916e995def7139d30
Stanislav Smirnov <stas.smirnov88@gmail.com>
2016-02-22 02:06:50 +0300

    delivery details start using full delivery object

--- app/controllers/deliveryDetails.js
+++ app/controllers/deliveryDetails.js
@@ -3,7 +3,10 @@
 angular.module('myApp')
     .controller('DeliveryDetailsController', ['$scope', '$state', '$stateParams',
         function ($scope, $stateParams) {
-            $scope.title = "Delivery 1";
+
+            $scope.delivery = $stateParams.delivery;
+
+            $scope.title = $scope.delivery.title;
 
             $scope.listItems = [{title: '4'}];
 
@@ -11,7 +14,7 @@ angular.module('myApp')
 
             $http({
                 method: "GET",
-                url: "http://169.45.106.72:8080/server/webapi/deliveries/" + $stateParams.deliveryId + '/comments'
+                url: "http://169.45.106.72:8080/server/webapi/deliveries/" + $scope.delivery._id + '/comments'
             }).then(function successCallback(response) {
                 $scope.comments = angular.fromJson(response.data).items;
                 // to do fill
