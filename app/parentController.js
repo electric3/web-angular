bb7298e6ef7e965df85ac712f65f645a876b0581
Stanislav Smirnov <stas.smirnov88@gmail.com>
2016-02-21 19:28:25 +0300

    tmp redirect from landing to dashboard

--- app/parentController.js
+++ app/parentController.js
@@ -3,7 +3,11 @@ angular.module('myApp')
     .controller('ParentController', ['$scope', '$window',
         function ($scope, $window) {
             $scope.redirectToDashboard = function () {
-                $window.location.href = $window.location.href.replace('index.html', 'views/dashboard.html');
+                if($window.location.href.indexOf("index.html") > 0) {
+                    $window.location.href = $window.location.href.replace('index.html', 'views/dashboard.html');
+                } else {
+                    $window.location.href += 'views/dashboard.html';
+                }
             };
         }
     ]);
