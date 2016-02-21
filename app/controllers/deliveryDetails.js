8bbe2948abe043d7604297981759ebb7614dae48
Stanislav Smirnov <stas.smirnov88@gmail.com>
2016-02-22 02:01:14 +0300

    delivery comments viewer

--- app/controllers/deliveryDetails.js
+++ app/controllers/deliveryDetails.js
@@ -1,10 +1,22 @@
 'use strict';
 
 angular.module('myApp')
-    .controller('DeliveryDetailsController', ['$scope', '$state',
-        function ($scope, $state) {
+    .controller('DeliveryDetailsController', ['$scope', '$state', '$stateParams',
+        function ($scope, $stateParams) {
             $scope.title = "Delivery 1";
 
             $scope.listItems = [{title: '4'}];
+
+            $scope.comments = [];
+
+            $http({
+                method: "GET",
+                url: "http://169.45.106.72:8080/server/webapi/deliveries/" + $stateParams.deliveryId + '/comments'
+            }).then(function successCallback(response) {
+                $scope.comments = angular.fromJson(response.data).items;
+                // to do fill
+            }, function errorCallback(response) {
+                console.log(response);
+            });
         }
     ]);
